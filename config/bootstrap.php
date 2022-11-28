ee22bb366d1b6c8a006330d7f18014992c926eee
iyaremye-faustin <iyaremyef@gmail.com>
2022-11-28 22:43:13 +0200

    fix-dev-design-rules

--- config/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("PROJECT_ROOT_PATH", __DIR__ . "/../");
- 
-require_once PROJECT_ROOT_PATH . "/config/config.php";
- 
-require_once PROJECT_ROOT_PATH . "/Controller/ResponseController.php";
- 
-require_once PROJECT_ROOT_PATH . "/Model/ProductModel.php";
